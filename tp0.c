8cff0f88142077450307c1ada8f88c5b73224230
c a r o l i n <54341921+csmamani@users.noreply.github.com>
2020-10-03 16:21:18 -0300

    Add files via upload

--- tp0.c
+++ tp0.c
@@ -18,16 +18,14 @@ void swap(int *x, int *y) {
 
 int maximo(int vector[], int n) {
 
-    int maximo = 0;
-    int posicion_maximo;
+    int posicion_maximo = 0;
 
     if (n == 0) {
         return -1;
     }
 
     for (int i = 0; i < n; i++) {
-        if(vector[i] > maximo) {
-            maximo = vector[i];
+        if(vector[i] > vector[posicion_maximo]) {
             posicion_maximo = i;
         }
     }
@@ -39,11 +37,33 @@ int maximo(int vector[], int n) {
 
 int comparar(int vector1[], int n1, int vector2[], int n2) {
 
-    for (int i = 0; i < n1; i++) {
-        while(vector1[i] != vector2[i]) {
-            if ( (vector1[i] < vector2[i]) || (!vector1[i] && vector2[i]) ) {
+    if (n1 != n2) {
+
+        if(n1 < n2) {
+        
+            for (int i = 0; i < n1; i++) {
+                if ( (vector1[i] > vector2[i])) {
+                    return 1;       
+                }                
+            }
+            return -1;
+            
+        } else if (n1 > n2) {
+        
+            for (int i = 0; i < n2; i++) {
+                if ( (vector1[i] < vector2[i]) ) {
+                    return -1;            
+                }              
+            }
+            return 1;
+        }        
+
+    } else {
+        
+        for (int i = 0; i < n1; i++) {
+            if ( (vector1[i] < vector2[i]) ) {
                 return -1;            
-            } else if ( (vector1[i] > vector2[i]) || (vector1[i] && !vector2[i]) ) {
+            } else if ( (vector1[i] > vector2[i])) {
                 return 1;       
             }
         }
@@ -58,11 +78,9 @@ void seleccion(int vector[], int n) {
 
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
-            
             if (vector[i] > vector[j]) {
                 swap(&vector[i], &vector[j]);
             }
-            
         }
     }
 
